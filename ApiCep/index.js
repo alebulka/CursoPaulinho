464d6650ae5026d98f52f4b8c16e255c8d15ed12
alebulka <bulkalessandra@outlook.com>
2023-04-04 21:03:03 -0300

    integrando js no html

--- ApiCep/index.js
+++ ApiCep/index.js
@@ -2,19 +2,19 @@ const axios = require ('axios');
 
 let cep = 83065400;
 
-const rua = document.getElementById('rua');
-/* const bairro = document.getElementById('bairro').value=(conteudo.bairro);
-const cidade = document.getElementById('cidade').value=(conteudo.localidade);
-const estado = document.getElementById('estado').value=(conteudo.uf); */
-
 async function pegaCep(cep) {
   try {
-    const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`)
-    const data = response.data
-    const rua = document.getElementById('rua');
-      console.log(data);
+    const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
+    const data = response.data;
+    console.log(document);
+    const rua = document.getElementById('rua').setAttribute('value', data.rua);
+    const bairro = document.getElementById('bairro').setAttribute('value', data.bairro);
+    const cidade = document.getElementById('cidade').setAttribute('value', data.cidade);
+    const estado = document.getElementById('estado').setAttribute('value', data.estado);
+   /*  rua.value = data.rua */
+
   } catch (error) {
     console.log(error);
   };
 }
-pegaCep(cep)
+pegaCep(cep);
