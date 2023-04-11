697b20f90837f8b9e1d6be823cbef6b42a5b31da
alebulka <bulkalessandra@outlook.com>
2023-04-11 09:59:02 -0300

    adicionando function valida cep

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# CursoPaulinho
+# Mentoria by Paulinho
+
+👩‍💻 Repositório de testes e desafios para maior conhecimento em JavaScript e Node.JS
+
+**O que estamos vendo:**
+* Consumir API da ViaCEP
+  * Fazer requisições usando AJAX / XMLHttpRequest
+
+##  Como rodar a Aplicação?
+
+
+* Clone este repositório na sua linha de comando
+```
+$ git clone https://github.com/alebulka/CursoPaulinho.git
+```
+
+* Instale as dependências
+```
+$ npm install
+```
+
+---
+
+Repositório criado para fins educativos
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const axios = require ('axios');
 
-let cep = 83065400;
+let cep = '83065-400';
+
+function validaCep() {
+  const cepReplace = cep.replace('-', '');
+  if (cepReplace.length == 8) {
+      console.log(cepReplace);
+  }
+  else{ console.log(`CEP "${cep}" format invalid`)}
+}
+validaCep();
 
 async function pegaCep(cep) {
   try {
     const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
     const data = response.data;
-    console.log(document);
+/*     console.log(document); */
     const rua = document.getElementById('rua').setAttribute('value', data.rua);
     const bairro = document.getElementById('bairro').setAttribute('value', data.bairro);
     const cidade = document.getElementById('cidade').setAttribute('value', data.cidade);
     const estado = document.getElementById('estado').setAttribute('value', data.estado);
-   /*  rua.value = data.rua */
-
   } catch (error) {
-    console.log(error);
+/*     console.log(error); */
   };
 }
 pegaCep(cep);
