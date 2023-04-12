b4fbe5859aa74a7e4dfb3af4198c40ae870d2584
alebulka <bulkalessandra@outlook.com>
2023-04-12 08:54:59 -0300

    xmlhttprequest para fazer requisições

--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
-const axios = require ('axios');
+const XMLHttpRequest = require('xhr2').XMLHttpRequest;
 
 let cep = '83065-400';
 
 function validaCep() {
   const cepReplace = cep.replace('-', '');
   if (cepReplace.length == 8) {
-      console.log(cepReplace);
+    console.log(cepReplace);
   }
-  else{ console.log(`CEP "${cep}" format invalid`)}
+  else{ console.log(`CEP "${cep}" format invalid`)};
 }
 validaCep();
 
-async function pegaCep(cep) {
+const req = new XMLHttpRequest();
+req.open("GET", `http://viacep.com.br/ws/${cep}/json/`);
+
+req.addEventListener("readystatechange", function() {
+  if(req.readyState === 4 && req.status == 200) {
+    req.onload = () =>{
+      const respostaApi = JSON.parse(req.response);
+      const rua = document.getElementById('rua').setAttribute('value', data.rua);
+      const bairro = document.getElementById('bairro').setAttribute('value', data.bairro);
+      const cidade = document.getElementById('cidade').setAttribute('value', data.cidade);
+      const estado = document.getElementById('estado').setAttribute('value', data.estado);
+    };
+  }
+});
+
+req.send();
+
+
+/* async function pegaCep(cep) {
   try {
     const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
-    const data = response.data;
-/*     console.log(document); */
-    const rua = document.getElementById('rua').setAttribute('value', data.rua);
-    const bairro = document.getElementById('bairro').setAttribute('value', data.bairro);
-    const cidade = document.getElementById('cidade').setAttribute('value', data.cidade);
-    const estado = document.getElementById('estado').setAttribute('value', data.estado);
-  } catch (error) {
-/*     console.log(error); */
-  };
-}
-pegaCep(cep);
+    const data = response.data; */
+    /*     console.log(document); */
+
+/*   } catch (error) {
+        console.log(error);
+  }; */
+/* }
+
+pegaCep(cep); */
