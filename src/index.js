f8d98787fcadd5222a35d3480779d916377f0047
alebulka <bulkalessandra@outlook.com>
2023-04-12 10:07:55 -0300

    código validando cep correto

--- src/index.js
+++ src/index.js
@@ -1,43 +1,37 @@
-const XMLHttpRequest = require('xhr2').XMLHttpRequest;
+function validaCep(){
+  
+  let cep = document.getElementById('cep').value;
 
-let cep = '83065-400';
-
-function validaCep() {
   const cepReplace = cep.replace('-', '');
+  let cepOK 
   if (cepReplace.length == 8) {
-    console.log(cepReplace);
+    cepOK = cepReplace;
   }
-  else{ console.log(`CEP "${cep}" format invalid`)};
+  else{ 
+    window.alert(`CEP "${cep}" format invalid`)
+  };
+  
+  const req = new XMLHttpRequest();
+  req.open("GET", `http://viacep.com.br/ws/${cepOK}/json/`);
+  req.send();
+  
+  req.addEventListener("readystatechange", function() {
+    if(req.readyState === 4 && req.status == 200) {
+      req.onload = () =>{
+        const respostaApi = JSON.parse(req.response);
+        if (respostaApi.erro == true) {
+          let rua = document.getElementById('rua').value = respostaApi.erro;
+          console.log('oooooi');
+/*           const bairro = document.getElementById('bairro').value = respostaApi.bairro;
+          const cidade = document.getElementById('cidade').value = respostaApi.localidade;
+          const estado = document.getElementById('estado').value = respostaApi.estado; */
+        }
+        let rua = document.getElementById('rua').value = respostaApi.logradouro;
+        let bairro = document.getElementById('bairro').value = respostaApi.bairro;
+        let cidade = document.getElementById('cidade').value = respostaApi.localidade;
+        let estado = document.getElementById('estado').value = respostaApi.estado;
+      };
+    } 
+  });
 }
 validaCep();
-
-const req = new XMLHttpRequest();
-req.open("GET", `http://viacep.com.br/ws/${cep}/json/`);
-
-req.addEventListener("readystatechange", function() {
-  if(req.readyState === 4 && req.status == 200) {
-    req.onload = () =>{
-      const respostaApi = JSON.parse(req.response);
-      const rua = document.getElementById('rua').setAttribute('value', data.rua);
-      const bairro = document.getElementById('bairro').setAttribute('value', data.bairro);
-      const cidade = document.getElementById('cidade').setAttribute('value', data.cidade);
-      const estado = document.getElementById('estado').setAttribute('value', data.estado);
-    };
-  }
-});
-
-req.send();
-
-
-/* async function pegaCep(cep) {
-  try {
-    const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
-    const data = response.data; */
-    /*     console.log(document); */
-
-/*   } catch (error) {
-        console.log(error);
-  }; */
-/* }
-
-pegaCep(cep); */
